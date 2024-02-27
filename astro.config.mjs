87bd07d9f16d53485d6045a0ef50d75e3884b4a8
seanleken <seanleken43@gmail.com>
2024-02-27 00:21:45 +0000

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
       exclude: ["oslo"]
     }
   },
-  output: "server"
+  output: "server",
+  adapter: vercel()
 });
